40c47e7a8a601f8a07c7929588c88264cc46ad20
MtHouks <raxs64@gmail.com>
2016-04-07 00:20:17 -0430

    Third commit

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,6 +1,12 @@
 $('#arrow_id').hide();
+$('#arrow_id2').hide();
+$('#arrow_id3').hide();
 $('.wrapper').click(function(event) {
 	$('#arrow_id').show('fade');
+	$('#arrow_id2').show('fade');
+	$('#arrow_id3').show('fade');
 	$('#arrow_id').addClass('GoUpBig');
+	$('#arrow_id2').addClass('GoUpMedium');
+	$('#arrow_id3').addClass('GoUpSmall');
 });
 
